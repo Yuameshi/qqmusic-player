f63abc458b5a50e5ca28b75f3867d4b07857c5f4
i-am-a-loser-using-windows-server <64469437+i-am-a-loser-using-windows-server@users.noreply.github.com>
2020-05-25 18:18:04 +0800

    完成了许久咕咕咕的歌名搜索（使用人工跨域），顺便改正了拼写错误

--- api.js
+++ api.js
@@ -11,16 +11,16 @@ window.onload=function(){
   setInterval(autofetchprogress,100);
 }
 
-function msprev(songname,singer,aubum,image) {
-  if(!aubum){
-    aubum = songname;
+function msprev(songname,singer,album,image) {
+  if(!album){
+    album = songname;
   }
   if ('mediaSession' in navigator){
     navigator.mediaSession.metadata = new MediaMetadata({
       title: songname,
       artist: singer,
-      album: aubum,
-      artwork: [{src: image, sizes: "200x200", type: "image/jpeg"}]
+      album: album,
+      artwork: [{src: image, sizes: "300x300", type: "image/jpeg"}]
     });
     /*navigator.mediaSession.setActionHandler('play', function() {});
     navigator.mediaSession.setActionHandler('pause', function() {});
@@ -47,37 +47,28 @@ function throughid() {
     iptbox.value = "";
     if (!ipt) {
       print('Error：Empty String！');
-      alert('曲目名/曲目ID不能为空！');
+      alert('曲目ID不能为空！');
       return;
     }
     print("Input Box Value：" + ipt);
     player.play(ipt);
-    var duration = "NaN";
-    
     print('Duration:' + duration);
-    /*if(duration == "NaN") {
+    if(duration == "NaN") {
       print("Error:Cannot Fetch Duration，Maybe Song Not Found!");
       alert('错误：无法获取曲目时长，可能没有该曲目！');
       return;
-    }*/
-
-    msprev(ipt,"Unknown Artist","Unknown Aubum","http://39.101.194.181/proj/qqmusic/throughid.jpg");
-    
+    }
+    msprev(ipt,"Unknown Artist","Unknown album","http://39.101.194.181/proj/qqmusic/throughid.jpg");
     var songname = document.getElementById("songname");
     songname.innerHTML = ipt;
-
     var title = document.getElementById("title");
     title.innerHTML = ipt + ' · QQ音乐播放器';
-
   	var singer = document.getElementById("singer");
   	singer.innerHTML = "未知艺术家";
-
-    var aubum = document.getElementById("aubum");
-    aubum.src = "throughid.jpg";
-
+    var album = document.getElementById("album");
+    album.src = "throughid.jpg";
     var songmid = document.getElementById("songmid");
     songmid.innerHTML = "曲目ID：" + ipt;
-
   	var time = document.getElementById("time");
     time.innerHTML = "0s/" + player.duration + "s";
 }
@@ -87,22 +78,27 @@ function throughid() {
 function throughname() {
 	  print("Start Running Process：throughname()");
 	  var iptbox = document.getElementById("iptbox");
+    var album = 'http://imgcache.qq.com/music/photo/album_200/20/200_albumpic_140820_0.jpg'
     var ipt = iptbox.value;
+    print("Input Box Value：" + ipt);
     iptbox.value = "";
     if (!ipt) {
       print('Error：Empty String！');
-      alert('曲目名/曲目ID不能为空！');
+      alert('曲目名不能为空！');
       return;
     }
-    var newwindow = 'https://y.qq.com/portal/search.html#page=1&searchid=1&remoteplace=txt.yqq.top&t=song&w=' + ipt;
-    alert('即将打开一个新窗口，请在新窗口内选中希望播放的歌曲并使用曲目ID播放！\n如：\nURL为“https://y.qq.com/n/yqq/song/000WKvUf0GuCyl.html”\n则取“000WKvUf0GuCy”');
-    window.open(newwindow,'newwindow','height=700,width=600,top=0,left=0,toolbar=no,menubar=no,scrollbars=no,resizable=no, status=no');
-	  /*var script = document.createElement("script")
-    script.src = `https://c.y.qq.com/soso/fcgi-bin/music_search_new_platform?searchid=53806572956004615&t=1&aggr=1&cr=1&catZhida=1&lossless=0&format=json&flag_qc=0&p=1&n=2&w=${iptbox.value}`;
-    print(script);
-    document.body.appendChild(script);
-    print("Input Box Value：" + iptbox.value);
-    alert('功能还没上线呢（咕咕咕）！');*/
+    var method = window.confirm("请选择希望的播放方式：\n是：将跳转到QQ音乐搜索API\n否：将弹出QQ音乐搜索界面\n\n注意：部分浏览器肯可能会拦截弹出窗口，届时请允许此页面弹出窗口！");
+    if (method == true) {
+      var newwindow = 'https://c.y.qq.com/soso/fcgi-bin/music_search_new_platform?searchid=53806572956004615&t=1&aggr=1&cr=1&catZhida=1&lossless=0&format=json&flag_qc=0&p=1&n=1&w=' + ipt;
+      alert('即将打开一个新窗口，请在新窗口内选中所有字符并拷贝至稍后的输入框内，若不正确输入则可能出现无法正确播放，歌曲信息无法正确显示等错误！');
+      window.open(newwindow,'newwindow','height=700,width=600,top=0,left=0,toolbar=no,menubar=no,scrollbars=no, status=no')
+      setTimeout(function(){var getted_json = prompt("请在此输入你接受到的JSON字符串：","");qmjsonstr(getted_json);},5000);
+    }
+    if (method == false) {
+      var newwindow = 'https://y.qq.com/portal/search.html#page=1&searchid=1&remoteplace=txt.yqq.top&t=song&w=' + ipt;
+      alert('即将打开一个新窗口，请在新窗口内选中希望播放的歌曲并使用曲目ID播放！\n如：\nURL为“https://y.qq.com/n/yqq/song/000WKvUf0GuCyl.html”\n则取“000WKvUf0GuCy”');
+      window.open(newwindow,'newwindow','height=700,width=600,top=0,left=0,toolbar=no,menubar=no,scrollbars=no, status=no');
+    }
 }
 
 //播放函数
@@ -163,7 +159,6 @@ function autofetchprogress() {
     var wrt = wrtcurr_min + ':0' + wrtcurr_sec + '/' + wrtdura_min + ':' +wrtdura_sec;
   } else {
     var wrt = wrtcurr_min + ':' + wrtcurr_sec + '/' + wrtdura_min + ':' +wrtdura_sec;
-
   }
   timecount.innerHTML = wrt;
   timecount.style.color = "#000";
@@ -209,4 +204,74 @@ function getMouseCoords(ev) {
         x: ev.clientX + document.body.scrollLeft - document.body.clientLeft,
         y: ev.clientY + document.body.scrollTop - document.body.clientTop
     };
+}
+
+//谁搞出了跨域可以用这里的函数
+function qmjsonraw(res) {
+  if(!res){
+    res = {"code":0,"data":{"album":{"curnum":0,"curpage":1,"list":[],"totalnum":0},"keyword":"","mv":{"curnum":0,"curpage":1,"list":[],"totalnum":0},"priority":0,"qc":[],"sematic":{"curnum":0,"curpage":1,"list":[],"totalnum":0},"song":{"curnum":0,"curpage":1,"list":[{"albumName_hilight":"王者荣耀 游戏原声 时之恋人","albumTransName":"Time Reflection /  /  / ","albumTransName_hilight":"<span class=\"c_tx_highlight\">Time Reflection</span> /  /  / ","chinesesinger":0,"docid":"4090721507775583551","f":"265138219|Time Reflection|1339126|王者荣耀;Jennifer Sumire|12424333|王者荣耀 游戏原声 时之恋人|0|268|0|1|5|10759217|4303945|0|0|0|30153146|5958804|6486615|0|004RKijy0HaQqd|002KASw53FQbTK|002nRQNR178Nzf|31|0","fiurl":"","fnote":0,"fsinger":"王者荣耀","fsinger2":"Jennifer Sumire","fsong":"Time Reflection","grp":[],"isupload":0,"isweiyun":0,"lyric":"","lyric_hilight":"","mv":"","nt":1263440265,"only":0,"pubTime":1589904012,"pure":0,"singerMID":"002KASw53FQbTK","singerMID2":"0022ChW82n1fcK","singerName2_hilight":"Jennifer Sumire","singerName_hilight":"王者荣耀","singerid":1339126,"singerid2":2903796,"songName_hilight":"<span class=\"c_tx_highlight\">Time Reflection</span>","t":1,"tag":10,"ver":0},{"albumName_hilight":"王者荣耀 游戏原声 时之恋人","albumTransName":"Time Reflection /  /  / ","albumTransName_hilight":"<span class=\"c_tx_highlight\">Time Reflection</span> /  /  / ","chinesesinger":0,"docid":"12539038946254143869","f":"265548078|Time Reflection|1339126|王者荣耀|12424333|王者荣耀 游戏原声 时之恋人|0|268|0|1|5|10759556|4303967|0|0|0|27486620|6208226|6485632|0|001f1OS63cRWl2|002KASw53FQbTK|002nRQNR178Nzf|31|0","fiurl":"","fnote":0,"fsinger":"王者荣耀","fsinger2":"","fsong":"Time Reflection (伴奏)","grp":[],"isupload":0,"isweiyun":0,"lyric":"","lyric_hilight":"","mv":"","nt":873189400,"only":1,"pubTime":1590041768,"pure":0,"singerMID":"002KASw53FQbTK","singerMID2":"","singerName2_hilight":"","singerName_hilight":"王者荣耀","singerid":1339126,"singerid2":0,"songName_hilight":"<span class=\"c_tx_highlight\">Time Reflection</span> (伴奏)","t":1,"tag":10,"ver":0}],"totalnum":0},"totaltime":0.0,"zhida":{"chinesesinger":0,"type":0,"zhida_album":{}}},"message":"no results","notice":"","subcode":10003,"time":1590381656,"tips":""}
+    }
+  print("JSON Data Parsing...")
+  var songmid = res.data.song.list["0"].media_mid;
+  var album = res.data.song.list["0"].albumname;
+  var albumid = res.data.song.list["0"].albumid;
+  var songname = res.data.song.list["0"].songname;
+  var singer1 = res.data.song.list["0"].singerName_hilight;
+  var singer2 = res.data.song.list["0"].singerName2_hilight;
+  if(singer2 != undefined){
+    var singer = singer1+';'+singer2;
+  }
+  print("Parsed SongMID : " + songmid);
+  print("Parsed Singer : " + singer);
+  print("Parsed album ID : " + albumid);
+  var album_element = document.getElementById("album");
+  var title_element = document.getElementById("title");
+  var songname_element = document.getElementById("songname");
+  var songid_element = document.getElementById("songmid");
+  var singer_element = document.getElementById("singer");
+  songname_element.innerHTML = songname;
+  singer_element.innerHTML = singer;
+  songid_element.innerHTML = '曲目ID：' + songmid;
+  title_element.innerHTML = songname + ' · QQ音乐播放器';
+  album_element.src = 'http://imgcache.qq.com/music/photo/album_300/20/300_albumpic_'+ res.data.song.list["0"].albumid +'_0.jpg';
+  player.play(songmid);
+  album_element.innerHTML = album;
+  msprev(songname , singer , album,"http://imgcache.qq.com/music/photo/album_300/20/300_albumpic_"+ albumid +"_0.jpg");
+}
+
+
+function qmjsonstr(res) {
+  if (!res) {
+    var res=prompt();
+  }
+  var temp;
+  var songname = res.slice(res.indexOf("fsong")+8,res.indexOf("grp")-3);
+  print("Parsed Song Name: " + songname);
+  var coredata = res.slice(res.indexOf("\"f\""), res.indexOf("\"fiurl\""));
+  console.log("Prised Core Data: " + coredata);
+  temp = songname.length + 15 + 9;
+  var singer = coredata.slice(temp,coredata.indexOf("|",temp+1));
+  print("Parsed Singer: " + singer);
+  temp = temp + singer.length + 10;
+  var album = coredata.slice(temp,coredata.indexOf("|",temp+1));
+  print("Parsed album Name: " + album);
+  var songmid = coredata.slice(coredata.indexOf("|00")+1,coredata.indexOf("|00")+15);
+  print("Prised SongMid: " + songmid);
+  temp = 15+songname.length+9+singer.length+1;
+  var albumid = coredata.slice(temp,coredata.indexOf("|",temp));
+  print("Prised Album ID: "+ albumid)
+
+  player.play(songmid);
+  var album_element = document.getElementById("album");
+  var title_element = document.getElementById("title");
+  var songname_element = document.getElementById("songname");
+  var songid_element = document.getElementById("songmid");
+  var singer_element = document.getElementById("singer");
+  songname_element.innerHTML = songname;
+  singer_element.innerHTML = singer;
+  songid_element.innerHTML = '曲目ID：' + songmid;
+  title_element.innerHTML = songname + ' · QQ音乐播放器';
+  album_element.src = 'http://imgcache.qq.com/music/photo/album_300/20/300_albumpic_' + albumid + '_0.jpg';
+  album_element.innerHTML = album;
+  msprev(songname, singer, album, "http://imgcache.qq.com/music/photo/album_300/20/300_albumpic_" + albumid + "_0.jpg");
 }
